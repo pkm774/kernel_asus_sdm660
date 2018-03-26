eac02c339578b93fa769c4d4c84f979d10d9f3c6
Alexander Martinz <alex@amartinz.at>
2018-03-26 11:37:27 +0200

    qcacld-3.0: fix compilation if not a module
    Change-Id: Iec1c88b2d17c71b9b3f9082f630ff5a8dec8b453
    Signed-off-by: Alexander Martinz <alex@amartinz.at>
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -12677,6 +12677,7 @@ dev_alloc_err:
 	return -ENODEV;
 }
 
+#ifdef MODULE
 static void wlan_hdd_state_ctrl_param_destroy(void)
 {
 	cdev_del(&wlan_hdd_state_cdev);
@@ -12686,6 +12687,7 @@ static void wlan_hdd_state_ctrl_param_destroy(void)
 
 	pr_info("Device node unregistered");
 }
+#endif
 
 /**
  * __hdd_module_init - Module init helper
