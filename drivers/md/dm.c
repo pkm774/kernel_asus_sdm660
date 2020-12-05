b656bb8d5afa58c6f681d252c5ccc2750c79a3bb
ultron14 <ultron1704@gmail.com>
2020-12-05 15:52:34 +0530

    sdm660: Fix not a structure or union shit
    ../drivers/md/dm.c: In function 'dm_init_old_md_queue':
    ../drivers/md/dm.c:2316:29: error: request for member 'congested_data' in something not a structure or union
      md->queue->backing_dev_info.congested_data = md;
                                 ^
    ../drivers/md/dm.c:2317:29: error: request for member 'congested_fn' in something not a structure or union
      md->queue->backing_dev_info.congested_fn = dm_any_congested;

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -2309,12 +2309,12 @@ static void dm_init_old_md_queue(struct mapped_device *md)
 {
 	md->use_blk_mq = false;
 	dm_init_md_queue(md);
-
+	
 	/*
 	 * Initialize aspects of queue that aren't relevant for blk-mq
 	 */
-	md->queue->backing_dev_info.congested_data = md;
-	md->queue->backing_dev_info.congested_fn = dm_any_congested;
+	md->queue->backing_dev_info->congested_data = md;
+	md->queue->backing_dev_info->congested_fn = dm_any_congested;
 	blk_queue_bounce_limit(md->queue, BLK_BOUNCE_ANY);
 }
 
