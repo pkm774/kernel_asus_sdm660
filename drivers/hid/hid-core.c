f4ba6ccf5dfc73c731f6e38472cbcc65c55fbe1f
pkm774 <mprabhat774@gmail.com>
2020-12-05 12:24:27 +0530

    sdm636: Fix commit 9976ef960bc61323d122ad38040e9fce7663c918 as hid_add_usage have only 2 arguments

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -220,14 +220,6 @@ static int hid_add_usage(struct hid_parser *parser, unsigned usage)
 	}
 	parser->local.usage[parser->local.usage_index] = usage;
 
-	/*
-	 * If Usage item only includes usage id, concatenate it with
-	 * currently defined usage page
-	 */
-	if (size <= 2)
-		complete_usage(parser, parser->local.usage_index);
-
-	parser->local.usage_size[parser->local.usage_index] = size;
 	parser->local.collection_index[parser->local.usage_index] =
 		parser->collection_stack_ptr ?
 		parser->collection_stack[parser->collection_stack_ptr - 1] : 0;
