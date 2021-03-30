21de6d41c4857aafea5f2d6a1861b2afec5e96b3
engstk <eng.stk@sapo.pt>
2021-03-30 12:17:56 +0530

    drivers: misc: implement usb fast charge mode
    echo 0 /sys/kernel/fast_charge/force_fast_charge (disable)
    echo 1 /sys/kernel/fast_charge/force_fast_charge (enable)
    
    Enables force charging up to 900mA in usb mode
    
    Change-Id: I991253eb6e41bb795abcf664c68c33a6075028e9
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -47,6 +47,10 @@
 #define	START_REPORT_BAT_TEMPRATURE	620
 /* Huaqin add for ZQL1650-76 Report battery temperature to upper by gaochao at 2018/02/07 end */
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define smblib_err(chg, fmt, ...)		\
 	pr_err("%s: %s: " fmt, chg->name,	\
 		__func__, ##__VA_ARGS__)	\
@@ -983,6 +987,13 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 	int rc;
 	u8 icl_options;
 	const struct apsd_result *apsd_result = smblib_get_apsd_result(chg);
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge > 0 && icl_ua == USBIN_500MA)
+	{
+		icl_ua = USBIN_900MA;
+	}
+#endif
 
 	/* power source is SDP */
 	switch (icl_ua) {
