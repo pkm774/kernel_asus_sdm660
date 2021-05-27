0b93d39a7fce8be5543f4d6a98c8d90d2d9c275c
pkm774 <mprabhat774@gmail.com>
2021-05-27 18:21:28 +0530

    sdm660: Update build scripts
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- builda.sh
+++ builda.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+# Input device & version
 read -p 'Enter device codename: ' DEVICE
 read -p 'Enter kernel upstreamed version: ' VERSION
 
@@ -12,49 +11,50 @@ echo ''
 echo Starting kernel build.
 echo ''
 
+# set tools directory path
 TOP=$(realpath ../)
 
-export KBUILD_BUILD_USER=pkm774
-export KBUILD_BUILD_HOST=ubuntu
+# export aosp toolchains path 
+export PATH="$TOP/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH"
+export PATH="$TOP/tools/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH"
+export PATH="$TOP/tools/clang/host/linux-x86/clang-r407598/bin:$PATH"
+export LD_LIBRARY_PATH="$TOP/tools/clang/host/linux-x86/clang-r407598/lib64:$LD_LIBRARY_PATH"
 
-PATH="$TOP/tools/build-tools/linux-x86/bin:$PATH"
-PATH="$TOP/tools/build-tools/path/linux-x86:$PATH"
-PATH="$TOP/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH"
-PATH="$TOP/tools/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH"
-PATH="$TOP/tools/clang/host/linux-x86/clang-r353983d/bin:$PATH"
-PATH="$TOP/tools/misc/linux-x86/lz4:$PATH"
-PATH="$TOP/tools/misc/linux-x86/dtc:$PATH"
-PATH="$TOP/tools/misc/linux-x86/libufdt:$PATH"
-export LD_LIBRARY_PATH="$TOP/tools/clang/host/linux-x86/clang-r353983d/lib64:$LD_LIBRARY_PATH"
+# export required kernel flags
+export ARCH=arm64
+export SUBARCH=ARM64
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
-rm -rf "$TOP/Output/$DEVICE-kernel"
+# Clean old builds
 rm -rf "$TOP/ZIPS/stock/Image.gz-dtb"
 rm -rf "$TOP/ZIPS/Kernel_ZIP/STOCK"
 
-make \
-    O=out \
-    ARCH=arm64 \
-    SUBARCH=ARM64 \
-    CC=clang \
-    HOSTCC=clang \
-    HOSTCXX=clang++ \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE=aarch64-linux-android- \
-    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-    ${DEVICE}_defconfig
-    
-make \
-    -j$(nproc --all) \
-    O=out \
-    ARCH=arm64 \
-    SUBARCH=ARM64 \
-    CC=clang \
-    HOSTCC=clang \
-    HOSTCXX=clang++ \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
-    CROSS_COMPILE=aarch64-linux-android- \
-    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+# start make builds
+make	O=out \
+	CC=clang \
+	AR=llvm-ar \
+	NM=llvm-nm \
+	LD=ld.lld \
+	STRIP=llvm-strip \
+	OBJDUMP=llvm-objdump \
+	OBJCOPY=llvm-objcopy \
+	${DEVICE}_defconfig
 
+
+time	make \
+	O=out \
+	CC=clang \
+	AR=llvm-ar \
+	NM=llvm-nm \
+	LD=ld.lld \
+	STRIP=llvm-strip \
+	OBJDUMP=llvm-objdump \
+	OBJCOPY=llvm-objcopy \
+	--jobs=5
+
+# Zipping kernel build
 FILE='out/arch/arm64/boot/Image.gz-dtb'
 DIR="$TOP/ZIPS"
 if [[ -f "$FILE" ]] 
@@ -63,24 +63,18 @@ if [[ -f "$FILE" ]]
    if [[ ! -d "$DIR" ]] 
       then
       echo ZIPS Folder not found !
-      echo Image.gz-dtb file is saved in $TOP/Output/$DEVICE-kernel folder.
+      echo Image.gz-dtb file is saved in out/arch/arm64/boot folder.
       exit 1
    else
    mkdir -p "$TOP/ZIPS/Kernel_ZIP/STOCK"
-   cp out/arch/arm64/boot/Image.gz-dtb "$TOP/Output/$DEVICE-kernel"
    cp out/arch/arm64/boot/Image.gz-dtb "$TOP/ZIPS/stock"
    cd "$TOP/ZIPS/stock"
    zip -r "$TOP/ZIPS/Kernel_ZIP/STOCK/ProjectInfinity_STOCK-v$VERSION.zip" *
-   echo ''
    echo ''
    echo 'Kernel Build Successful !!!!'
    fi
 else
 echo ''
-echo ''
 echo 'Kernel build Unsuccessfull !!!!'
 echo ''
 fi
-
-
-
