9caab68fc1f10ac03d9c20e960000ba0e67ccccc
pkm774 <mprabhat774@gmail.com>
2021-08-01 15:30:07 +0530

    drivers: camerav2: Merge stock Kernel Changes
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
+++ drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
@@ -32,7 +32,9 @@
 #define CYCLES_PER_MICRO_SEC_DEFAULT 4915
 #define CCI_MAX_DELAY 1000000
 
-#define CCI_TIMEOUT msecs_to_jiffies(100)
+/* Huaqin modify for fixing cci timeout by lizihao at 2018/04/03 start*/
+#define CCI_TIMEOUT msecs_to_jiffies(1000)
+/* Huaqin modify for fixing cci timeout by lizihao at 2018/04/03 end*/
 
 /* TODO move this somewhere else */
 #define MSM_CCI_DRV_NAME "msm_cci"
